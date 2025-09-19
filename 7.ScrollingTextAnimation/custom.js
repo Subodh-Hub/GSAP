c27fd72e9155909b774807a32ab52e1751814ccb
Subodh-Hub <subodhrijal07@gmail.com>
2025-09-19 12:52:43 +0545

    Started back again with the gsap

---
+++ 7.ScrollingTextAnimation/custom.js
@@ -0,0 +1,28 @@
+const marqueAnimation = () => {
+  window.addEventListener("wheel", (dets) => {
+    console.log("dets: ", dets);
+    if (dets.deltaY > 0) {
+      gsap.to(".marque", {
+        transform: "translateX(-200%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: 180,
+      });
+    } else {
+      gsap.to(".marque", {
+        transform: "translateX(0%)",
+        duration: 4,
+        repeat: -1,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: 0,
+      });
+    }
+  });
+};
+
+marqueAnimation();
